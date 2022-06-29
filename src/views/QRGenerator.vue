8cc351abf885469738a9aee3b5ddb17ee39e8e2e
AKHIL <akhilennem@gmail.com>
2022-06-29 21:23:29 +0530

    first commit

---
+++ src/views/QRGenerator.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="bg-gray-50">
+        <!-- <form @submit.prevent="getQRcode">
+            <input type="text" v-model="QRValue">
+        </form> -->
+        <qrcode-vue v-if="QRValue" :value="QRValue" :size="300" />
+        
+    </div>
+</template>
+
+<script>
+import QrcodeVue from 'qrcode.vue';
+import axios from 'axios';
+
+export default {
+    data() {
+        return{
+            QRValue: "",
+        }
+    },
+    components: {
+        QrcodeVue
+    },
+     mounted() {
+       this.getQRcode();
+     },
+     methods:{
+      getQRcode(){
+    
+      axios.post('http://localhost:8081/api/rest/qr-generator')
+      .then((response)=>{
+        this.QRValue=response.data;
+        console.log(response.status);
+        console.log(response.data);
+        console.log("...................");
+      }).catch((error)=>{
+        console.log(error);
+      })          
+     }
+      },
+}
+</script>
